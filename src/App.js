0773320e34a81d8aa901954f1249fc0d367039a8
Maximilian Hammes <maximview@gmail.com>
2024-07-18 12:22:57 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>HALLO! LOREM IPSUM!</h1>
       </header>
+      <main>
+        <nav className="button-container">
+          <button className="button" id="button-1">
+            TEST 1
+          </button>
+          <button className="button" id="button-2">
+            TEST 2
+          </button>
+          <button className="button" id="button-3">
+            TEST 3
+          </button>
+        </nav>
+        <section className="main-container"></section>
+        <aside></aside>
+      </main>
+      <footer>
+        <br />
+        &copy; 2024 Petrosilius Zwackelmann. Alle Rechte vorbehalten.
+        <br />
+      </footer>
     </div>
   );
 }
